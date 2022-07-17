e19c7edf8af93000171c37ae3989a5678428deea
Shantanu Mukherjee <shantanumukherjee@Shantanus-MacBook-Pro.local>
2022-07-18 00:07:59 +0530

    computation

--- Computation/Main.py
+++ Computation/Main.py
@@ -1,15 +1,45 @@
 import requests
 import json
 import pandas as pd
+import warnings
 
+
+# This will be the URL where load data happens
 URL = 'https://fc3fd324-c2b4-45bd-95fc-c249d599d031.mock.pstmn.io/'
 
-def loadData():
-    response = requests.get(URL)
-    data = json.loads(response.text)
-    table = pd.DataFrame()
-    for key,value in data.items():
-        for k,v in value.items():
-            print(v)
+warnings.filterwarnings("ignore")
 
-loadData()
+""" it will take JSON response from the request URL and put it in the dataframe """
+
+class Computation:
+    def loadData(self, URL):
+        response = requests.get(URL)
+        data = json.loads(response.text)
+        table = pd.DataFrame(data)
+        table = self.compute(table)
+        return table
+
+    """ perform computation and adding in the table 
+
+        some formulaes
+            Net Profit Margin = net income / total revenue
+            Current Ratio = current assets / current liabilities
+    """
+
+    def compute(self, table):
+        netProfitMarginRow = []
+        operatingCashflowByNetProfitRow = []
+        currentRatioRow = []
+        for (columnKeys, columnValues) in table.iteritems():
+            netProfitMarginRow.append(
+                columnValues['Net Income']/columnValues['totalRevenue']*100)
+
+            operatingCashflowByNetProfitRow.append(
+                columnValues['Operating cashflow']/columnValues['Net Income'])
+
+            currentRatioRow.append(
+                columnValues['Current Assets']/columnValues['Current Liabilities'])
+        table.loc['Net Profit Margin'] = netProfitMarginRow
+        table.loc['Operating Cashflow / Net Profit'] = operatingCashflowByNetProfitRow
+        table.loc['Current Ratio'] = currentRatioRow
+        return table
---
+++ Computation/Readme.txt
@@ -0,0 +1,49 @@
+Input json format : 
+
+{
+	“2017” : {
+		
+			totalRevenue : 1000,
+			EBITA : 50,
+			PBIT : 100,
+			PBT : 10,
+			Net Income : 1,
+			EPS : 0.5,
+			Payout Ratio : 0.2,
+			Operating cashflow : 1,
+			Net changes in cash : 2,
+			Free cashflow : 10,
+			Current Assets: 10,
+			Non Current Assets: 1,
+			Total Assets: 200,
+			Current Liabilities: 1,
+			Non Current Liabilities: 1001,
+			Total Liabilities: 1,
+			Total Equity: 111,
+			Total Liabilities & Shareholder's Equity : 100,
+			Total Common Shares Outstanding: 1101
+			},
+
+“2018” : {
+		
+			totalRevenue : 1000,
+			EBITA : 50,
+			PBIT : 100,
+			PBT : 10,
+			Net Income : 1,
+			EPS : 0.5,
+			Payout Ratio : 0.2,
+			Operating cashflow : 1,
+			Net changes in cash : 2,
+			Free cashflow : 10,
+			Current Assets: 10,
+			Non Current Assets: 1,
+			Total Assets: 200,
+			Current Liabilities: 1,
+			Non Current Liabilities: 1001,
+			Total Liabilities: 1,
+			Total Equity: 111,
+			Total Liabilities & Shareholder's Equity : 100,
+			Total Common Shares Outstanding: 1101
+			}
+}
